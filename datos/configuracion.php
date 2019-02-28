3adf2ce671dc75a53feb3b930b69cc057749f9a9
erikgf <43494469+erikgf@users.noreply.github.com>
2019-02-27 19:06:23 -0500

    Update configuracion.php

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -8,8 +8,8 @@ define("PUERTO_BD", "5433");
 define("NOMBRE_BD", "bd_cayalti_agri");
 */
 define("TIPO_BD","postgres"); //postgres,mysql
-define("SERVIDOR_BD", "ec2-54-225-68-133.compute-1.amazonaws.com");
-define("USUARIO_BD", "upcsapkmmjtcie");
-define("CLAVE_BD", "948c8eb874fcdd1e41228b652d8b76e3dd51c81b0a5bd0170f3026a3f5c56585");
+define("SERVIDOR_BD", "ec2-54-83-44-4.compute-1.amazonaws.com");
+define("USUARIO_BD", "ubyahxxuqkxnxl");
+define("CLAVE_BD", "d2823e3196488a042719f71de4ebbc9642b3107bebefa50f0d661f09112bc3ab");
 define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "dbu6cbrqmluv6t");
+define("NOMBRE_BD", "d26e8kkfjotl9d");
