87f4ff153a4ff7f3002b546a29d8fd1406e7ef9c
erikgf <43494469+erikgf@users.noreply.github.com>
2019-03-06 11:06:04 -0500

    Update local_config_web.php

--- datos/local_config_web.php
+++ datos/local_config_web.php
@@ -9,7 +9,13 @@
  define("MODELO", "../negocio");
  define("MODELO_UTIL", MODELO."/util");
  define("MODELO_FUNCIONES",MODELO_UTIL."/Funciones.php");
- 
+
+ define("SW_NOMBRE","AgriCayaltí");
+ define("SW_NOMBRE_COMPLETO","Sistema de Biometría y Sanidad CAYALTI");
+ define("SW_VERSION","2.2.0 BETA");
+
+ define("MODO_PRODUCCION", 1);
+
  //SESION
  define("_SESION_","_sanidad_cayalti_web_");
  session_name(_SESION_);
