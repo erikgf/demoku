0f015962f3d5765bd8b5870ae5458d60e47a59dd
erikgf <43494469+erikgf@users.noreply.github.com>
2019-03-10 10:49:24 -0500

    Update ReporteadorFormulario.clase.php

--- negocio/ReporteadorFormulario.clase.php
+++ negocio/ReporteadorFormulario.clase.php
@@ -10,7 +10,7 @@ class ReporteadorFormulario extends Conexion {
 
             //la evaluacion debe TENER un fecha_inicio_Evalaucion_temporal y Estado Confirmado.
             $data = [];
-            $sql = "SELECT * FROM v_registros_diatraea WHERE date(raw_fecha_evaluacion) BETWEEN date(:0) AND date(:1)" ;
+            $sql = "SELECT * FROM v_registros_resumen_diatraea WHERE date(raw_fecha_evaluacion) BETWEEN date(:0) AND date(:1)" ;
             $data["diatraea"] = $this->consultarFilas($sql, [$fi, $ff]);
 
             return ["rpt"=>true,"data"=>$data];
