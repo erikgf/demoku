8a7caad9db87386cdcb4c5cc27fdef55018b4523
erikgf <43494469+erikgf@users.noreply.github.com>
2019-04-02 14:54:04 -0500

    Update ReporteadorFormulario.clase.php

--- negocio/ReporteadorFormulario.clase.php
+++ negocio/ReporteadorFormulario.clase.php
@@ -4,10 +4,8 @@ require_once '../datos/Conexion.clase.php';
 
 class ReporteadorFormulario extends Conexion {
 
-
     public function obtenerReporte($fi, $ff){
         try {
-
             //la evaluacion debe TENER un fecha_inicio_Evalaucion_temporal y Estado Confirmado.
             $data = [];
             $sql = "SELECT * FROM v_registros_resumen_diatraea WHERE date(raw_fecha_evaluacion) BETWEEN date(:0) AND date(:1) 
@@ -16,6 +14,28 @@ class ReporteadorFormulario extends Conexion {
 	            numero_nivel_2,
 	              NULLIF(regexp_replace(numero_nivel_3, '\D', '', 'g'), '')::integer" ;
             $data["diatraea"] = $this->consultarFilas($sql, [$fi, $ff]);
+
+            $sql = "SELECT * FROM v_full_resumen_diatraea 
+                    WHERE date(fecha_evaluacion) BETWEEN date(:0) AND date(:1) ORDER BY nombre_campo";
+
+            $data["diatraea_resumen"] = $this->consultarFilas($sql, [$fi, $ff]);
+
+            $sql = "SELECT 
+                    r.* , p.area
+                    FROM v_registros_carbon r
+                    INNER JOIN parcela p ON p.cod_parcela = r.cod_parcela
+                    WHERE date(raw_fecha_evaluacion) BETWEEN date(:0) AND date(:1) ORDER BY 
+                    nombre_campo,
+                    p.numero_nivel_1, 
+	            p.numero_nivel_2,
+	              NULLIF(regexp_replace(p.numero_nivel_3, '\D', '', 'g'), '')::integer" ;
+            $data["carbon"] = $this->consultarFilas($sql, [$fi, $ff]);
+
+
+            $sql = " SELECT * FROM v_full_resumen_carbon
+                     WHERE date(fecha_evaluacion) BETWEEN date(:0) AND date(:1) ORDER BY nombre_campo";
+
+            $data["carbon_resumen"] = $this->consultarFilas($sql, [$fi, $ff]);
 
             return ["rpt"=>true,"data"=>$data];
 
